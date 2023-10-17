3d698107b14c90e8021b05490460179ab55c3053
jsn-bln <jaysonbalano99@gmail.com>
2023-10-16 20:53:27 -0400

    setup mongodb connection on server

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
-
-
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 
 const app = express();
+app.use(express.json());
+
+
+
 const port = process.env.port || 8080;
+const uri = process.env.URI;
+
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection;
+db.once('open', () => { console.log('connected to mongodb!')})
 
 
 
