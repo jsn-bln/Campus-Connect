ea3aa47408610d656f5d4e24a86d3add08499d05
Jayson <jaysonbalano99@gmail.com>
2024-01-12 12:43:22 -0500

    added item schema

---
+++ model/itemSchema.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const itemSchema = mongoose.Schema({
+
+    studentId: {
+        type: Number,
+        required: true
+    },
+    itemName: {
+        type: String,
+        required: true,
+        maxLength: 20
+    },
+    itemDescription: {
+        type: String,
+        required: true,
+        maxLength: 200
+    },
+    itemPrice: {
+        type: Number,
+        required: true
+    },
+    itemQuantity: {
+        type: Number,
+        required: true
+    },
+    itemCondition: {
+        type: String,
+        enum: ['New', 'Used','Like New'],
+        required: true
+    },
+    datePosted: {
+        type: Date,
+        default: Date.now
+    },
+    itemStatus: {
+        type: String,
+        enum: ['Available', 'Sold'],
+        default: 'Available'
+    },
+
+});
+
+
+
+const Item = mongoose.model('Item', itemSchema);
+module.exports = Item;
